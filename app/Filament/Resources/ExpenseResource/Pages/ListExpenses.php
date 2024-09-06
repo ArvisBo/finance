f41a6ebc3366d7206627b5f8e12697bd2c767d1d
arvisb <arvis.bozenkovs@inbox.lv>
2024-09-07 02:51:47 +0300

    Added Tabs for Income and Expenses resource

--- app/Filament/Resources/ExpenseResource/Pages/ListExpenses.php
+++ app/Filament/Resources/ExpenseResource/Pages/ListExpenses.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\ExpenseResource\Pages;
 
 use App\Filament\Resources\ExpenseResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListExpenses extends ListRecords
 {
@@ -14,6 +16,18 @@ class ListExpenses extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+            'This Month' => Tab::make()
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('expense_date', '>=', now()->subMonth())),
+            'Six Month' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('expense_date', '>=', now()->subMonth(6))),
+            'Year' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('expense_date', '>=', now()->subYear())),
+            'All'=>Tab::make()
         ];
     }
 }
--- app/Filament/Resources/IncomeResource/Pages/ListIncomes.php
+++ app/Filament/Resources/IncomeResource/Pages/ListIncomes.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\IncomeResource\Pages;
 
 use App\Filament\Resources\IncomeResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListIncomes extends ListRecords
 {
@@ -14,6 +16,19 @@ class ListIncomes extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'This Month' => Tab::make()
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('income_date', '>=', now()->subMonth())),
+            'Six Month' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('income_date', '>=', now()->subMonth(6))),
+            'Year' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('income_date', '>=', now()->subYear())),
+            'All'=>Tab::make()
         ];
     }
 }
